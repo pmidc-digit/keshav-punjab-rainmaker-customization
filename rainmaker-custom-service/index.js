5e8d81325f416fc4fb393c245575da864a8b2b84
ABHISHEK  RANA <121286986+abhirana04@users.noreply.github.com>
2024-03-06 16:50:23 +0530

    Update index.js

--- rainmaker-custom-service/index.js
+++ rainmaker-custom-service/index.js
@@ -1052,7 +1052,7 @@ router.post('/protected/punjab-pt/pre-hook/pg-service/transaction/v1/_create', a
         url_callback.path = '/customization/open/punjab-pt/payu/confirm';
         url_callback.pathname = '/customization/open/punjab-pt/payu/confirm';
 
-        request['Transaction']['callbackUrl'] = url.format(url_callback);
+        request['Transaction']['callbackUrl'] = url.format(original_callback);
     }
      // else if (request['Transaction']['tenantId'] == 'pb.amritsar' || request['Transaction']['tenantId'] == 'pb.mohali' || request['Transaction']['tenantId'] == 'pb.hoshiarpur' || request['Transaction']['tenantId'] == 'pb.kapurthala' || request['Transaction']['tenantId'] == 'pb.khanna' || request['Transaction']['tenantId'] == 'pb.moga' || request['Transaction']['tenantId'] == 'pb.mandigobindgarh'|| request['Transaction']['tenantId'] == 'pb.handiaya') {
     else {
